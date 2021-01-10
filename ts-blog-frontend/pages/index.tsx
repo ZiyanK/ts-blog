255d3b160310b7b073f68f8228f0968b0cf9ca10
ZiyanK <ziyankarmali786@gmail.com>
2021-01-10 17:06:09 +0530

    Added next js frontend folder with typescript

---
+++ ts-blog-frontend/pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+	return (
+		<div className={styles.container}>
+			Welcome to Ziyan's Blog website
+		</div>
+	)
+}
---
+++ ts-blog-frontend/pages/profile/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.css";
+
+export default function Profile() {
+	return(
+		<div className={styles.container}>
+			<h1 className={styles.header}>Welcome to the profile page</h1>
+		</div>
+	)
+}
